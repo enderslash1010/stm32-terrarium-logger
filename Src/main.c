0f2a6cefc4348499e5e26cf7f3defdba3a3b4ff8
enderslash1010 <64379743+enderslash1010@users.noreply.github.com>
2023-11-08 19:32:44 -0600

    Removed function calls in IRQs that use SPI DMA
     - Interrupts cannot be triggered in interrupts (without enabling global interrupts, best to avoid if possible), and since SPI DMA uses interrupts, functions that send data over SPI cannot be called from an ISR
     - Created display_wake() and display_sleep() to improve readability
     - Added if blocks in the superloop to detect the cases when display_wake() and display_sleep() need to be called
     - Moved display_init() to be next to display_wake() and display_sleep()

--- Src/main.c
+++ Src/main.c
@@ -31,31 +31,6 @@ void clock_init(void)
 	while (!(RCC->CFGR & RCC_CFGR_SWS_PLL)); // Wait for clock source to be set
 }
 
-// Set up parts of the screen that don't change
-void display_init(void)
-{
-	static const char degreeSign[] = {0x80, 0x0}; // Degree sign character (0x80 in default font, ending in null terminator)
-	static const char plusMinusSign[] = {0x81, 0x0}; // Plus-minus sign character (0x81)
-
-	pcd8544_set_cursor(&screen, 0, 0);
-	pcd8544_write_string(&screen, "Terrarium 1:");
-	pcd8544_set_cursor_string(&screen, 4, 1);
-	pcd8544_write_string(&screen, degreeSign);
-	pcd8544_write_string(&screen, "F ");
-	pcd8544_set_cursor_string(&screen, 9, 1);
-	pcd8544_write_string(&screen, plusMinusSign);
-	pcd8544_write_string(&screen, "2%");
-
-	pcd8544_set_cursor(&screen, 0, 3);
-	pcd8544_write_string(&screen, "Terrarium 2:");
-	pcd8544_set_cursor_string(&screen, 4, 4);
-	pcd8544_write_string(&screen, degreeSign);
-	pcd8544_write_string(&screen, "F ");
-	pcd8544_set_cursor_string(&screen, 9, 4);
-	pcd8544_write_string(&screen, plusMinusSign);
-	pcd8544_write_string(&screen, "2%");
-}
-
 // Initiates button on PB0
 void button_init(void)
 {
@@ -95,15 +70,54 @@ void timer2_init(void)
 	NVIC->ISER[0] = (1 << 28); // Enable Interrupts for TIM2 in NVIC; TIM2 uses interrupt 28, so the enable bit is in ISER[0] at bit 28
 }
 
+// Set up parts of the screen that don't change
+void display_init(void)
+{
+	static const char degreeSign[] = {0x80, 0x0}; // Degree sign character (0x80 in default font, ending in null terminator)
+	static const char plusMinusSign[] = {0x81, 0x0}; // Plus-minus sign character (0x81)
+
+	pcd8544_set_cursor(&screen, 0, 0);
+	pcd8544_write_string(&screen, "Terrarium 1:");
+	pcd8544_set_cursor_string(&screen, 4, 1);
+	pcd8544_write_string(&screen, degreeSign);
+	pcd8544_write_string(&screen, "F ");
+	pcd8544_set_cursor_string(&screen, 9, 1);
+	pcd8544_write_string(&screen, plusMinusSign);
+	pcd8544_write_string(&screen, "2%");
+
+	pcd8544_set_cursor(&screen, 0, 3);
+	pcd8544_write_string(&screen, "Terrarium 2:");
+	pcd8544_set_cursor_string(&screen, 4, 4);
+	pcd8544_write_string(&screen, degreeSign);
+	pcd8544_write_string(&screen, "F ");
+	pcd8544_set_cursor_string(&screen, 9, 4);
+	pcd8544_write_string(&screen, plusMinusSign);
+	pcd8544_write_string(&screen, "2%");
+}
+
+void display_wake(void)
+{
+	pcd8544_set_display_control(&screen, 1, 0); // Screen in normal mode
+	pcd8544_toggle_backlight(&screen); // Turn on backlight
+	display_init(); // Set up static parts of screen
+	displayOn = 1;
+}
+
+void display_sleep(void)
+{
+	pcd8544_set_display_control(&screen, 0, 0); // Set display blank
+	pcd8544_toggle_backlight(&screen); // Turn off backlight
+	displayOn = 0;
+}
+
 // EXTI0 Interrupt Handler, for wake button to turn on display
 void EXTI0_IRQHandler(void)
 {
 	if (displayOn == 0) // Wake display if it's off
 	{
-		pcd8544_set_display_control(&screen, 1, 0); // Screen in normal mode
-		pcd8544_toggle_backlight(&screen); // Turn on backlight
-		display_init();
-		displayOn = 1;
+		// See discussion in TIM2_IRQHandler() for why display_sleep() is not called there, same applies for display_wake()
+		// The superloop checks whether TIM2 is enabled when displayOn = 0, which then calls display_wake()
+		// Unlike the TIM2 ISR, there is no noticeable lag time between pressing the button and display_wake()
 
 		// Set timer for how long to leave screen on, when timer ends turn the display back off
 		TIM2->CNT = 0; // Make sure count is at 0
@@ -120,12 +134,15 @@ void EXTI0_IRQHandler(void)
 void TIM2_IRQHandler(void)
 {
 	TIM2->CR1 &= ~TIM_CR1_CEN; // Disable TIM2 counter
+	// Not best to call display_sleep() from here because it sends SPI to screen, which uses interrupts
+	// Since we are currently in an interrupt, another interrupt cannot be triggered without enabling global interrupts, which is best to avoid if not needed
+	// Therefore, the superloop checks whether TIM2 is disabled when displayOn = 1, which then calls display_sleep()
 
-	// Put display to sleep
-	pcd8544_set_display_control(&screen, 0, 0); // Set display blank
-	pcd8544_toggle_backlight(&screen); // Turn off backlight
-	displayOn = 0;
-
+	// This has the side effect of having a lag time between the timer interrupt and display_sleep(), because the rest of the superloop needs to be executed before the display turns off
+	// This lag time can theoretically range from 0 to just over 1 second
+	// The delay_ms(1000) is where the program counter (pc) will be at most of the time, the 2nd biggest 'time waster' is temp/humidity sensors taking 15ms each loop (and SPI with the display is 546us)
+	// Since this interrupt has an equal chance of being tirggered from anywhere in the superloop, the expected value of this lag time is around 0.5 seconds
+	// For this context, the lag time is acceptable
 	TIM2->SR = ~TIM_SR_UIF; // Clear Update Interrupt Flag by writing 0 (rc_w0)
 }
 
@@ -164,8 +181,7 @@ int main(void)
 	timer2_init(); // Initialize timer 2
 
 	screen = pcd8544_init(GPIOA, RST_PIN, DC_PIN, BL_PIN, Vcc_PIN, 0x70, 0, 0b011); // Initialize screen
-	pcd8544_toggle_backlight(&screen); // Turn on screen backlight
-	displayOn = 1;
+	display_wake();
 
 	SHT30_t sensor1 = sht30_init(I2C1, 36000000, 0x44); // Initialize sensor 1 on I2C1
 	SHT30_t sensor2 = sht30_init(I2C2, 36000000, 0x44); // Initialize sensor 2 on I2C2
@@ -178,16 +194,19 @@ int main(void)
 	char temperature1Str[5], humidity1Str[3]; // Initialize string buffers for temp and humidity from sensor 1
 	char temperature2Str[5], humidity2Str[3]; // Initialize string buffers for temp and humidity from sensor 2
 
-	// Set up parts of the screen that don't change upon startup
-	display_init();
-
-	// Start TIM2 to turn off the display after 10 sec, since the display is already on upon start
+	// Start TIM2 to turn off the display after ~10 sec, since the display is already on upon start
 	TIM2->CR1 |= TIM_CR1_CEN;
 
     while (1)
     {
     	if (displayOn) // Update display if on
     	{
+    		if (!(TIM2->CR1 & TIM_CR1_CEN)) // If timer 2 is off
+    		{
+    			display_sleep();
+    			continue; // continues execution back at while (1), skipping everything below
+    		}
+
         	sht30_get_raw_sensor(&sensor1, 1, 1, sensor1Data); // Get temp and humidity values from sensor 1
         	sht30_get_raw_sensor(&sensor2, 1, 1, sensor2Data); // Get temp and humidity values from sensor 2
 
@@ -224,6 +243,10 @@ int main(void)
         	pcd8544_write_string(&screen, humidity2Str);
 
         	delay_ms(1000);
+    	}
+    	else if (TIM2->CR1 & TIM_CR1_CEN) // If displayOn = 0 and timer 2 is on
+    	{
+    		display_wake();
     	}
     }
 }
